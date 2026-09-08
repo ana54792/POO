8d199b37f1acb941c5d98ad83f69db821d6ab31a
Ana Raquel Nóbrega <kekelnosantos@gmail.com>
2026-09-08 17:15:57 +0000

    correção

--- Projeto/models/horario.py
+++ Projeto/models/horario.py
@@ -1,43 +1,40 @@
 from datetime import datetime
 
 class Horario:
-    def __init__(self, id, data, conf, id_c, id_s):
+    def __init__(self, id, data):
         self.set_id(id)
         self.set_data(data)
-        self.set_confirmado(conf)
-        self.set_id_cliente(id_c)
-        self.set_id_servico(id_s)
-    
-    def set_id(self, id):
-        if id < 0: raise ValueError("Id deve ser positivo")
-        self.__id = id
-    def set_data(self, data):
-        if data < datetime.now(): raise ValueError("Data deve ser no futuro")
-        self.__data = data
-    def set_confirmado(self, conf):
-        if conf == False: raise ValueError("Confirmação não foi feita")
-        self.__confirmado = conf
-    def set_id_cliente(self, id_c):
-        if id_c < 0: raise ValueError("Id deve ser positivo")
-        self.__id_cliente = id_c
-    def set_id_servico(self, id_s):
-        if id_s < 0: raise ValueError("Id deve ser positivo")
-        self.__id_servico = id_s
-        
-                
-
-    def get_id(self) : return self.__id
-    def get_data(self) : return self.__data
-    def get_confirmado(self) : return self.__confirmado
-    def get_id_cliente(self) : return self.__id_cliente
-    def get_id_servico(self) : return self.__id_servico
+        self.set_confirmado(False)
+        self.set_id_cliente(0)
+        self.set_id_servico(0)
 
     def __str__(self):
-        return f"{self.__id} - {datetime.strftime(self.__data, "%d/%m/%Y, %H:%M")} - {self.__confirmado} - {self.__id_cliente} - {self.__id_servico}"
-    
+        return f"{self.__id} - {self.__data.strftime('%d/%m/%Y %H:%M')} - {self.__confirmado}"
+
+    def get_id(self): return self.__id
+    def get_data(self): return self.__data
+    def get_confirmado(self): return self.__confirmado
+    def get_id_cliente(self): return self.__id_cliente
+    def get_id_servico(self): return self.__id_servico
+
+    def set_id(self, id): self.__id = id
+    def set_data(self, data): self.__data = data
+    def set_confirmado(self, confirmado): self.__confirmado = confirmado
+    def set_id_cliente(self, id_cliente): self.__id_cliente = id_cliente
+    def set_id_servico(self, id_servico): self.__id_servico = id_servico
+
     def to_json(self):
-        return { "id":self.__id, "data": datetime.strftime(self.__data, "%d/%m/%Y, %H:%M"), "confirmado":self.__confirmado, "id_cliente":self.__id_cliente, "id_servico":self.__id_servico }
-    
+        dic = {"id":self.__id, "data":self.__data.strftime("%d/%m/%Y %H:%M"), \
+            "confirmado":self.__confirmado, "id_cliente":self.__id_cliente,   \
+            "id_servico":self.__id_servico
+        }
+        return dic
+
     @staticmethod
     def from_json(dic):
-        return Horario(dic["id"], dic["data"], dic["confirmado"], dic["id_cliente"], dic["id_servico"])
+        horario = Horario(dic["id"], datetime.strptime(dic["data"], "%d/%m/%Y %H:%M"))
+        horario.set_confirmado(dic["confirmado"])
+        horario.set_id_cliente(dic["id_cliente"])
+        horario.set_id_servico(dic["id_servico"])
+        return horario
+    
--- Projeto/templates/manterclienteui.py
+++ Projeto/templates/manterclienteui.py
@@ -23,7 +23,7 @@ class ManterClienteUI:
         nome = st.text_input("Informe o nome")
         email = st.text_input("Informe o e-mail")
         fone = st.text_input("Informe o fone")
-        if st.button("Cadastrar", key="cadastro_cliente"):
+        if st.button("Inserir"):
             Service.cliente_inserir(nome, email, fone)
             st.success("Cliente inserido com sucesso")
             time.sleep(2)
@@ -36,7 +36,7 @@ class ManterClienteUI:
             nome = st.text_input("Novo nome", op.get_nome())
             email = st.text_input("Novo e-mail", op.get_email())
             fone = st.text_input("Novo fone", op.get_fone())
-            if st.button("Atualizar", key="atualizar_cliente"):
+            if st.button("Atualizar"):
                 id = op.get_id()
                 Service.cliente_atualizar(id, nome, email, fone)
                 st.success("Cliente atualizado com sucesso")
@@ -47,7 +47,7 @@ class ManterClienteUI:
         if len(clientes) == 0: st.write("Nenhum cliente cadastrado")
         else:
             op = st.selectbox("Exclusão de Clientes", clientes)
-            if st.button("Excluir", key="excluir_cliente"):
+            if st.button("Excluir"):
                 id = op.get_id()
                 Service.cliente_excluir(id)
                 st.success("Cliente excluído com sucesso")
